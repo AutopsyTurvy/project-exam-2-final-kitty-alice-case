335234da364db65c1de8e463add8410640c292e2
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2025-01-28 18:57:52 +0100

    parallax scroll messing

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@
 // App.jsx -- Note- acting as landing page. 
 
 
-
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
@@ -16,7 +15,6 @@ import BookingsPage from "./pages/BookingsPage";
 import Venues from "./pages/Venues";
 import VenueDetails from "./pages/VenueDetails";
 
-
 // Parallax images:
 import backgroundImage from "./assets/Parallax/background.png";
 import hillhouseImage from "./assets/Parallax/hillhouse.png";
@@ -24,10 +22,7 @@ import chimneysmokeImage from "./assets/Parallax/chimneysmoke.png";
 import foregroundImage from "./assets/Parallax/foreground.png";
 import formerpurplehillsImage from "./assets/Parallax/formerpurplehills.png";
 import latterpurplehillsImage from "./assets/Parallax/latterpurplehills.png";
-import deserthillsImage from "./assets/Parallax/deserthills.png"; 
-import sunImage from "./assets/Parallax/sun.png"; 
-import sunsethazeImage from "./assets/Parallax/sunsethaze.png"; 
-
+import deserthillsImage from "./assets/Parallax/deserthills.png";
 
 // Imported page styles:
 import "./styles/landingheader.css";
@@ -41,52 +36,35 @@ import UnregisteredHeader from "./components/UnregisteredHeader";
 // Imports the conditional header functions:
 import { isUserLoggedIn, getUserProfile } from "./api/IsRegistered";
 
+
+
+
+
 function LandingPage() {
-  const navigate = useNavigate();
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
 
-  const handleNavigateToRegister = () => {
-    navigate("/register");
-  };
+      document.documentElement.style.setProperty("--purple-hills-offset", `${scrollPosition * 0.2}px`); 
+      document.documentElement.style.setProperty("--house-offset", `${scrollPosition * 0.3}px`); 
+      document.documentElement.style.setProperty("--smoke-offset", `${scrollPosition * 0.4}px`); 
+    };
 
-  const handleNavigateToLogin = () => {
-    navigate("/login");
-  };
+    window.addEventListener("scroll", handleScroll);
 
-  const handleNavigateToVenues = () => {
-    navigate("/venues");
-  };
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div>
       <div className="landing-container">
-        {/* Background layer */}
         <img src={backgroundImage} alt="Background" className="landing-image background-layer" />
-
-        {/* Sunset Haze layer */}
-        <img src={sunsethazeImage} alt="Sunset Haze" className="landing-image sunsethaze-layer" />
-
-        {/* Sun layer */}
-        <img src={sunImage} alt="Sun" className="landing-image sun-layer" />
-
-        {/* Desert Hills layer */}
         <img src={deserthillsImage} alt="Desert Hills" className="landing-image deserthills-layer" />
-
-        {/* Latter Purple Hills layer */}
         <img src={latterpurplehillsImage} alt="Latter Purple Hills" className="landing-image latterpurplehills-layer" />
-
-        {/* Former Purple Hills layer */}
         <img src={formerpurplehillsImage} alt="Former Purple Hills" className="landing-image purplehills-layer" />
-
-        {/* Hillhouse layer */}
         <img src={hillhouseImage} alt="Hillhouse" className="landing-image hillhouse-layer" />
-
-        {/* Chimney smoke layer */}
         <img src={chimneysmokeImage} alt="Chimney Smoke" className="landing-image chimneysmoke-layer" />
-
-        {/* Foreground layer */}
         <img src={foregroundImage} alt="Foreground" className="landing-image foreground-layer" />
-
-        {/* Central header */}
         <div className="central-header">
           <h1>Where will we travel together?</h1>
         </div>
@@ -94,6 +72,10 @@ function LandingPage() {
     </div>
   );
 }
+
+
+
+
 
 function App() {
   const isLoggedIn = isUserLoggedIn();
@@ -110,19 +92,20 @@ function App() {
 
       {/* All of our Main Routes */}
       <Routes>
-        <Route path="/" element={<LandingPage />} /> {/* --Landing Page */}
-        <Route path="/login" element={<LoginPage />} /> {/* --Login Page */}
-        <Route path="/register" element={<RegisterPage />} /> {/* --Registration Page */}
-        <Route path="/profile/:username" element={<ProfilePage />} /> {/* --Profile Page */}
-        <Route path="/bookings" element={<BookingsPage />} /> {/* --Bookings Page */}
-        <Route path="/venues" element={<Venues />} /> {/* --Venues Page */}
-        <Route path="/venue/:id" element={<VenueDetails />} /> {/* --To see the venue details */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+        <Route path="/bookings" element={<BookingsPage />} />
+        <Route path="/venues" element={<Venues />} />
+        <Route path="/venue/:id" element={<VenueDetails />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
